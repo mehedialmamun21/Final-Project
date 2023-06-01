495781477de41af0d59238e32f83f0fb925914d1
mehedialmamun21 <mehedialmamuncse@gmail.com>
2023-06-02 04:08:37 +0600

    all data displaying in table

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Data from "./Data.json"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <table className="table">
+        <thead>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Email</th>
+        </thead>
+        <tbody>
+          {Data.map((d, i) => (
+            <tr key={i}>
+              <td>{d.ID}</td>
+              <td>{d.Name}</td>
+              <td>{d.Email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
