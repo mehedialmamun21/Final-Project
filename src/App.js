76f096ef6492ae14035bb9f23ec4211f901fbb01
mehedialmamun21 <mehedialmamuncse@gmail.com>
2023-06-02 04:57:36 +0600

    pagination successfully done for table data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import Data from "./Data.json"
 
 function App() {
+
+  const [currentPage, setCurrentPage] = useState(1)
+  const recordsPerPage = 5;
+  const lastIndex = currentPage * recordsPerPage;
+  const firstIndex = lastIndex - recordsPerPage;
+  const records = Data.slice(firstIndex, lastIndex);
+  const npage = Math.ceil(Data.length / recordsPerPage);
+  const numbers = [...Array(npage + 1).keys()].slice(1)
+
   return (
     <div>
       <table className="table">
@@ -11,7 +20,7 @@ function App() {
           <th>Email</th>
         </thead>
         <tbody>
-          {Data.map((d, i) => (
+          {records.map((d, i) => (
             <tr key={i}>
               <td>{d.ID}</td>
               <td>{d.Name}</td>
@@ -20,8 +29,42 @@ function App() {
           ))}
         </tbody>
       </table>
-    </div>
-  );
+      <nav>
+        <ul className="pagination">
+          <li className="page-item">
+            <a href="#" className="page-link"
+              onClick={prePage}>Prev</a>
+          </li>
+          {
+            numbers.map((n, i) => (
+              <li className={`page-item ${currentPage === n ? 'active' : ''}`} key={i} >
+                <a href="#" className="page-link"
+                  onClick={() => changeCPage(n)} >{n}</a>
+              </li>
+            ))
+          }
+          <li className="page-item">
+            <a href="#" className="page-link"
+              onClick={nextPage}>Next</a>
+          </li>
+        </ul>
+
+      </nav>
+    </div >
+  )
+  function prePage() {
+    if (currentPage !== 1) {
+      setCurrentPage(currentPage - 1)
+    }
+  }
+  function changeCPage(id) {
+    setCurrentPage(id)
+  }
+  function nextPage() {
+    if (currentPage !== npage) {
+      setCurrentPage(currentPage + 1)
+    }
+  }
 }
 
 export default App;
